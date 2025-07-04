b600f18903d75ff8ddcdf4be98a03bbfd885fe17
Erik Peteri <peteri.erik@gmail.com>
2025-07-04 10:07:01 +0300

    Initialize project structure

---
+++ main.py
@@ -0,0 +1,7 @@
+from metropolis_hastings import MetropolisHastings
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ metropolis_hastings.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class MetropolisHastings:
+    def __init__(self):
+        pass
+
+    def uniform_distribution(self):
+        return
