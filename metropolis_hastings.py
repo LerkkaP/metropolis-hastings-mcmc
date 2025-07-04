71b4f483bb960d3fd295ac2fafe7bae77f391a97
Erik Peteri <peteri.erik@gmail.com>
2025-07-04 10:27:30 +0300

    Create visualizer class skeleton

--- metropolis_hastings.py
+++ metropolis_hastings.py
@@ -1,8 +1,13 @@
 import numpy as np
+import random
 
 class MetropolisHastings:
     def __init__(self):
-        pass
+        self.states_accepted = []
+        self.states_rejected = []
 
     def uniform_distribution(self):
-        return
+        pass
+    
+    def acceptance_probability(self):
+        pass
---
+++ visualizer.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+class Visualizer:
+    @staticmethod
+    def trace_plot():
+        pass
+
+    def histogram():
+        pass
